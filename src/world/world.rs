c77a6e95392b04450b5a70aa7ef7577ba5933c1c
AStatt-Desktop <AStatt@Protonmail.com>
2022-08-06 12:50:06 -0700

    Add basic route for render tests

--- src/world/world.rs
+++ src/world/world.rs
@@ -9,15 +9,41 @@ pub struct World {
 }
 
 impl World {
-
+    pub fn new() -> World {
+        World {
+            current_route: Route::new(),
+        }
+    }
+    pub fn current_route(&self) -> &Route {return &self.current_route}
 }
 
 
-#[derive(Copy, Clone)]
-struct Route {
-    
+#[derive(Clone)]
+pub struct Route {
+    size: glam::IVec2, //Starts at 0
+    tiles: Vec<i32>, 
 }
 
-
+impl Route {
+    pub fn new() -> Route {
+        let tiles = vec![
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 5, 5, 5, 5, 5, 5, 5, 0,
+            0, 5, 5, 5, 5, 5, 5, 5, 0,
+            0, 5, 5, 5, 5, 5, 5, 5, 0,
+            0, 5, 5, 5, 5, 5, 5, 5, 0,
+            0, 5, 96, 5, 5, 5, 5, 5, 0,
+            0, 5, 5, 5, 5, 5, 5, 5, 0,
+            0, 5, 5, 5, 5, 5, 5, 5, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0
+        ];
+        Route{
+            size: glam::IVec2::new(8,8),
+            tiles,
+        }
+    }
+    pub fn size(&self) -> glam::IVec2 {return self.size}
+    pub fn tiles(&self) -> &Vec<i32> {return &self.tiles} 
+}
 
 
