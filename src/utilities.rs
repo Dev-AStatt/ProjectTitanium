e47d6b6bf2a332144c6dff7b550ea4e2a8ffe03d
AStatt-Desktop <AStatt@Protonmail.com>
2022-08-12 16:12:18 -0700

    added direction enum to utility

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,9 +1,16 @@
 use ggez::{Context, graphics};
 
+#[derive(Copy, Clone)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 //Function will take a path to the sprite that wants to load and return either
 //that sprite or a red cube of 5x5 if there was an error
 pub fn load_image(ctx: &Context, path: &str) -> graphics::Image {
-    
     let img = graphics::Image::from_path(ctx, path, true);
 
     match graphics::Image::from_path(ctx, path, true) {
