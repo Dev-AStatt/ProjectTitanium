5c1115ab75b1d4848ece114def5dc4f93057ae6f
AStatt-Desktop <AStatt@Protonmail.com>
2022-08-12 14:32:32 -0700

    added functionality to player

--- src/world/player.rs
+++ src/world/player.rs
@@ -1,15 +1,38 @@
 
 use glam;
 
+#[derive(Copy, Clone)]
+pub enum PlayerAction {
+    Idle,
+    Walking,
+    Running,
+}
+
+#[derive(Copy, Clone)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+
 pub struct Player {
     pos: glam::IVec2,
+    action: PlayerAction,
+    direction: Direction,
 }
 
 impl Player {
     pub fn new() -> Player {
         Player {
-            pos: glam::IVec2::new(500,500),
+            pos: glam::IVec2::new(10,10),
+            action: PlayerAction::Idle,
+            direction: Direction::Down,
         }
     }
+    pub fn current_action(&self) -> PlayerAction {return self.action}
+    pub fn position(&self) -> glam::IVec2 {return self.pos}
+    pub fn direction(&self) -> Direction {return self.direction}
 }
 
