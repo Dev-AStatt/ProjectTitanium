fabe909ea02f0386e540d0ef32cf4cc95b7e1391
AStatt-Desktop <AStatt@Protonmail.com>
2022-08-15 13:45:13 -0700

    Fixed - Json Importing tile properties into game

--- src/world/json_importer.rs
+++ src/world/json_importer.rs
@@ -1,3 +1,16 @@
+/* ASTATT - 2022-08-15
+* File holds code for importing Json files from the Tild Map editor.
+* Tild will export the map allong with the properties for the tileset
+* including things like custom properties and Class that all can be used
+* by our game to have tile properties be kept in a visual system instead of a 
+* big vector of intagers. 
+*
+* There are some gat-chas in here so take a look at the notes left for methods 
+* and I hope that when properties need to be added or this needs to be expanded to 
+* include multiple layers it should go ok. I have done my best to do some of the 
+* work to not make it super locked into one layer. 
+*/
+
 extern crate serde_json;
 extern crate serde;
 extern crate serde_derive;
@@ -18,6 +31,7 @@ pub enum TileClass {
 
 
 #[derive(serde_derive::Deserialize, Debug)]
+//Top Layer of the Imported Json file, holds everything
 pub struct ImportedRouteJson {
     compressionlevel: i8,
     layers: Vec<ImportedLayers>,
@@ -27,10 +41,15 @@ pub struct ImportedRouteJson {
 }
 impl ImportedRouteJson {
     pub fn layer(&self, u: usize) -> &ImportedLayers {return &self.layers[u]}
+    //get number of tilesets that were imported, used to check to make sure we only use 1
     pub fn num_tilesets(&self) -> usize {return self.tilesets.len()}
     pub fn tilesets(&self) -> &Vec<ImportedTilesets> {return &self.tilesets}
 }
 #[derive(serde_derive::Deserialize, Debug)]
+//Struct of a single layer that is imported.
+//data is the vector of GLobal_Tile_ID's from top left to bottom right.
+//data values are +1 to what is the actual position of the tile on 
+//the tilesheet. This is so tiled can export 0 if no tile exists
 pub struct ImportedLayers {
     data: Vec<i32>,
     height: u32,
@@ -39,19 +58,33 @@ pub struct ImportedLayers {
 impl ImportedLayers {
     pub fn data_by_ref(&self) -> &Vec<i32> {return &self.data}
     pub fn data_len(&self) -> usize {return self.data.len()}
+    pub fn width(&self) -> u32 {return self.width}
+    pub fn height(&self) -> u32 {return self.height}
 }
 
-
 #[derive(serde_derive::Deserialize, Debug)]
+//Tiled can export the information about the tileset used to build the map. 
+//THis can include custom properties, in our case if a tile is walkable or spanws pokemon.
+//the firstgid property is the id of the tileset. So should be 1. 
 pub struct ImportedTilesets {
     firstgid: u32,
     pub tiles: Vec<ImportedTile>,
 }
 impl ImportedTilesets {
+    //NOTE i: Usize used in this IMPL does NOT refer to the GLobal_Tile_ID
+    //that is used in the map gen. The i used here is the id of the imported tile vector 
+    //that contains ONLY the tiels with data in them. so it will jump from 
+    //GLobal_Tile_ID: 15 to 50 in terms of ImportedTile.ID 
+    //Use get_tiles_vecid_from_global to convert
+
+    //returns the number of tiles that were imported into the sheet
     pub fn num_tiles_in_imported_sheet(&self) -> usize {return self.tiles.len()}
     pub fn firstgid(&self) -> u32 {self.firstgid}
+    //return walkable flag of vector ID. 
     pub fn is_tile_walkable(&self, i: usize) -> bool {self.tiles[i].flag_val("Walkable".to_string())}
     pub fn is_tile_spawner(&self, i: usize) -> bool {self.tiles[i].flag_val("Spawn".to_string())}
+    //Function will return enum of tileclass from the imported String taht 
+    //tild exports the Class property in. Match with known used tileClass and print error if missed
     pub fn get_tile_class(&self, i: usize) -> TileClass {
         
         if i > self.tiles.len() {println!("ERROR HERE I: {} > Tile Length {}", i, self.tiles.len())}
@@ -64,6 +97,18 @@ impl ImportedTilesets {
         println!("Jaon Import Tile Error - Found unknown class: {} at ID: {}", str, i);
         return TileClass::Normal;
     }
+    //as explained above in the Note, you must convert the GLobal_Tile_ID to 
+    //the vector[i] to reference properties of a tile because of the jumps in imported tile_ids
+    pub fn get_tiles_vecid_from_global(&self, glob: i32) -> usize {
+        //search in all tiles that were imported into vector tiles
+        for i in 0..self.tiles.len() {
+            //if tile id matches imported global id - 1 
+            //(to correct for GLobal_Tile_ID + 1 json export)
+            if self.tiles[i].id() == (glob - 1) as usize {return i}
+        }
+        println!("Json Import Error - Get_Tile_VecID {}, Not Found", glob);
+        return 0;
+    }
 }
 
 
@@ -75,6 +120,7 @@ pub struct ImportedTile {
 }
 
 impl ImportedTile {
+    pub fn id(&self) -> usize {return self.id as usize}
     //checks through the vetor of properties for flag defined as string
     pub fn flag_val(&self, f: String) -> bool {
         //for each property for importedTiles
@@ -95,13 +141,6 @@ pub struct ImpProperties {
     pub value: bool,
 }
 
-
-impl ImportedLayers {
-    pub fn data(&self) -> Vec<i32> {return self.data.clone()}
-    pub fn width(&self) -> u32 {return self.width}
-    pub fn height(&self) -> u32 {return self.height}
-}
-
 pub fn read_json_from_file<P: AsRef<Path>>(path: P)
     -> Result<ImportedRouteJson, Box<dyn Error>> {
 
@@ -110,9 +149,6 @@ pub fn read_json_from_file<P: AsRef<Path>>(path: P)
     let u = serde_json::from_reader(reader)?;
     Ok(u)
 }
-
-
-
 
 #[cfg(test)]
 mod tests {
--- src/world/route.rs
+++ src/world/route.rs
@@ -33,11 +33,11 @@ pub struct Route {
 impl Route {
     pub fn new() -> Route {
 
+        //import Json file for route 
         let import_route = json_importer::read_json_from_file(
-            "../../resources/routes/titanium_town1_V02.json").unwrap();
-        
+            "../resources/routes/titanium_town1_V02.json").unwrap();
+        //Build the vector of tiles from imported Json
         let tiles = build_tiles_from_import(&import_route);
-
         let route_size = glam::IVec2::new(
             import_route.layer(0).width() as i32,
             import_route.layer(0).height() as i32
@@ -48,49 +48,63 @@ impl Route {
             tiles,
         }
     }
-
-
-
-
     pub fn size(&self) -> glam::IVec2 {return self.size}
     pub fn tiles(&self) -> &Vec<Tile> {return &self.tiles} 
     pub fn tile_id_at(&self, i: usize) -> i32 {return self.tiles[i].global_id()}
 
 }
 
+//Function handeles the translation from importing the Tiled json export into the 
+//usable structs that we need to use in the game. Currently this is building a 
+//vector of tiles Vec<Tile> that will be our map.
 fn build_tiles_from_import(imp_route: &ImportedRouteJson) -> Vec<Tile> {
+    //Written - ASTATT - 2022-08-15
+    //NOTE on Global_ID, global_ID is the tilesheet id + 1 because Tiled will export 0 for no tile
+
+    //create empty vector
     let mut tiles: Vec<Tile> = vec![];
+    //we will assume only one layer for now. to be changed later
     let layer = 0;
-    let num_tilesets = imp_route.num_tilesets();
+    //Tild lets you have multple tilesets for each "route" or map. We will try not
+    //to do this, so we get how many tile sets we have imported and make sure its only "1"
+    //else print an error message to remind us of this
+    if imp_route.num_tilesets() > 1 {println!("ERROR in Route.rs - Build Tiles from Import not setup for multple tilesets")}
+    let tile_set_to_import = imp_route.num_tilesets() - 1;
 
-    println!("{:?}",imp_route.tilesets()[0].tiles);
+    //for each global id that was imported into data. This is the bulk of what tile is where in a
+    //2d vector. we have to specify the layer (always 0) that we want the data from.
     for i in 0..imp_route.layer(layer).data_len() {
-       
+        //get the global ID of the tile at index i. this is what tile on the sheet is there.
         let global_id = imp_route.layer(layer).data_by_ref()[i];
-        println!("GlobalID: {}", global_id);
-        let tileset_id: usize = global_id as usize - 1;
-        let class = imp_route.tilesets()[0].get_tile_class(tileset_id); 
-        let walkable = imp_route.tilesets()[0].is_tile_walkable(tileset_id); 
-        let spawner = imp_route.tilesets()[0].is_tile_spawner(tileset_id); 
+        //So the tilesheet export json will only contain data for the tiles that actually have an
+        //image associated with them. So we will ask the imported tileset class to get us the
+        //vector id of the imported tile associated with the global id that we are looking for data
+        //at. 
+        //Example. All global ID's of 2 will get a return of vector id 1 but later tiles say 50
+        //will have a lower id, because tiles after 15 dont exist. 
+        let tileset_id = imp_route.tilesets()[tile_set_to_import]
+            .get_tiles_vecid_from_global(global_id);
+        //The next three methods are all the same type of call, use the tileset_id to get what data
+        //is associated with that tile. 
+        let class = imp_route.tilesets()[tile_set_to_import]
+            .get_tile_class(tileset_id); 
+        let walkable = imp_route.tilesets()[tile_set_to_import]
+            .is_tile_walkable(tileset_id); 
+        let spawner = imp_route.tilesets()[tile_set_to_import]
+            .is_tile_spawner(tileset_id); 
 
+        //make a new tile struct using what we have learned
         let new_tile = Tile::new(
             global_id,
             class,
             walkable,
             spawner,
         );
-        println!("Properly imported {:?}", new_tile);
+        //push new tile into vector that we are creating
         tiles.push(new_tile);
     }
-
     return tiles;
 }
-
-//    global_id: i32,
-//    class: TileClass,
-//    walkable: bool,
-//    spawn: bool,
-
 
 
 
